5ac70beebc8ba0b1989e5c64cf9bab4433a78754
khaled365-code <khaleddortmound123@gmail.com>
2022-08-27 20:12:12 +0200

    Deleting recycle view

--- app/src/main/java/com/khaledmohamed/am_clinics/doctor/DoctorData.kt
+++
@@ -1,9 +0,0 @@
-package com.khaledmohamed.am_clinics.doctor
-
-data class DoctorData (
-        val img: Int,
-        val name: String,
-        val spec: String,
-        val fees: Int
-        ){
-}
--- app/src/main/java/com/khaledmohamed/am_clinics/select_service_Activity.kt
+++ app/src/main/java/com/khaledmohamed/am_clinics/select_service_Activity.kt
@@ -8,11 +8,45 @@ import android.widget.Toast
 import com.khaledmohamed.am_clinics.databinding.ActivitySelectServiceBinding
 
 class select_service_Activity : AppCompatActivity() {
+
     lateinit var binding:ActivitySelectServiceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivitySelectServiceBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.selectappointmentChoose.setOnClickListener{
+
+            var intent=Intent(this,SpecialityActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"Please select an speciality ",Toast.LENGTH_LONG).show()
+
+        }
+
+        binding.selectcallsChoose.setOnClickListener {
+
+            var intent=Intent(this,SpecialityActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"Please select an speciality ",Toast.LENGTH_LONG).show()
+        }
+
+        binding.selectvisitChoose.setOnClickListener {
+
+            var intent=Intent(this,Doctor__or_nurse_Activity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"Please choose doctor or nurse ! ",Toast.LENGTH_LONG).show()
+
+        }
+
+        binding.selectreadingChoose.setOnClickListener {
+
+            var intent=Intent(this,Scroll_articles_Activity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"Choose an article to read it",Toast.LENGTH_LONG).show()
+        }
+
+
 
 
 
