bc7b4ba39cde3404869cda61e981eaac1cb812d1
khaled365-code <khaleddortmound123@gmail.com>
2022-08-27 02:55:11 +0200

    adding recycler design

--- app/src/main/java/com/khaledmohamed/am_clinics/Recyclerview_Activity.kt
+++ app/src/main/java/com/khaledmohamed/am_clinics/Recyclerview_Activity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class Recyclerview_Activity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recyclerview)
