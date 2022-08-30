5ddf346ffaab3f228ea1661f5983c8e43ecb3805
khaled365-code <khaleddortmound123@gmail.com>
2022-08-30 11:24:56 +0200

    New commit

--- app/src/main/java/com/khaledmohamed/am_clinics/LoginActivity.kt
+++ app/src/main/java/com/khaledmohamed/am_clinics/LoginActivity.kt
@@ -1,22 +1,35 @@
 package com.khaledmohamed.am_clinics
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import com.khaledmohamed.am_clinics.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityLoginBinding
+    lateinit var sharedpreferences:SharedPreferences
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        sharedpreferences=getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
         binding.btnLogin.setOnClickListener{
+            val name:String=binding.edtUsername.text.toString()
+            val editor:SharedPreferences.Editor=sharedpreferences.edit()
+            editor.putString("NAME",name)
+            editor.apply()
+            Toast.makeText(this,"YOUR USER NAME IS ${name} HELLO ${name}",Toast.LENGTH_LONG).show()
+
          if(!binding.edtUsername.text.isNullOrEmpty() && !binding.edtPassword.text.isNullOrEmpty())
          {
+
              var intent = Intent(this, select_service_Activity::class.java)
              startActivity(intent)
              finish()
@@ -34,4 +47,6 @@ class LoginActivity : AppCompatActivity() {
         }
 
     }
+
+
 }
